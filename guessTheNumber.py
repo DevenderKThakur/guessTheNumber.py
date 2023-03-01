2d24e9d3ead473537892b1ca0c9fb5817efee654
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-03-01 19:03:38 +0530

    Update guessTheNumber.py

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -12,9 +12,26 @@ def guessTheNumber(x):
 
     print (f'Congrats the number is {random_number}')      
 
-guessTheNumber(10)      
+# guessTheNumber(10)       Un -comment if you want to run the programme 
 
 
+def guessTheNumberByPC(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if(low != high ):
+          guess = random.randint(low, high) 
+        else:
+            guess = low   
+        feedback = input(f'Is {guess }  too high(h) ,too low(l) , or correct(c)').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback =='l':
+            low = guess+1
+    print(f'Congrats the numebr {guess}')    
+
+ # guessTheNumberByPC(300)  
 
 
     
